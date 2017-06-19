0831fcb045f3f988c2ff3e40fb93574232b31fef
DK\msb <msb@sitecore.net>
2017-06-19 12:13:14 -0400

    SupportInteractionHistoryRangeDefintion object has been made public

--- src/Sitecore.Support.137656/Analytics/Aggregation/History/SupportInteractionHistoryRangeDefinition.cs
+++ src/Sitecore.Support.137656/Analytics/Aggregation/History/SupportInteractionHistoryRangeDefinition.cs
@@ -3,13 +3,13 @@ using Sitecore.Analytics.DataAccess;
 
 namespace Sitecore.Support.Analytics.Aggregation.History
 {
-  internal class SupportInteractionHistoryRangeDefinition : ObjectRangeDefinition
-  {
-    public SupportInteractionHistoryRangeDefinition(DateTime cutoff)
+    public class SupportInteractionHistoryRangeDefinition : ObjectRangeDefinition
     {
-      CutoffDate = cutoff;
-    }
+        public SupportInteractionHistoryRangeDefinition(DateTime cutoff)
+        {
+            CutoffDate = cutoff;
+        }
 
-    public DateTime CutoffDate { get; private set; }
-  }
+        public DateTime CutoffDate { get; private set; }
+    }
 }
